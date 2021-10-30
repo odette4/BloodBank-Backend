46c024a83b12b62afd1baaf584a33c4daf22d1e2
odette <odethdusabe@gmail.com>
2021-10-30 12:23:48 +0200

    bloodbank donation

--- Server/Models/userModel.js
+++ Server/Models/userModel.js
@@ -1,39 +1,49 @@
 import mongoose from "mongoose";
 
-const UserSchema=new mongoose.Schema({
-Name:{type:String,
-require:[true, "name is required"]
-},
+const UserSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        require: [true, "name is required"]
+    },
 
-gender:{type:String,
-enum:["female","male"]},
+    gender: {
+        type: String,
+        enum: ["female", "male"]
+    },
 
-// dob: {type: Date, require:true},
+    // dob: {type: Date, require:true},
 
-// bloodGroup: {type:String, enum:['O','A','B','AB']},
+    // bloodGroup: {type:String, enum:['O','A','B','AB']},
 
-address: {type: String, required:true},
+    address: { type: String },
 
 
-phone: {type:String, require:true},
+    phone: { type: String, require: true },
 
-email: {type:String, 
-    require:true,
-    unique:true
-},
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
 
-password:{type:String,
-default:12345},
+    password: {
+        type: String,
+        default: 12345
+    },
 
-role:{type:String,
-    enum:["user","hospital","admin"],
-    default:"user"},
+    role: {
+        type: String,
+        enum: ["user", "hospital", "admin"],
+        default: "user"
+    },
 
-status:{type:String,
-enum:["active","inactive"]}
+    status: {
+        type: String,
+        enum: ["active", "inactive"]
+    }
 
 });
 
-const UserInfo=mongoose.model('User',UserSchema);
+const UserInfo = mongoose.model('User', UserSchema);
 
 export default UserInfo;
